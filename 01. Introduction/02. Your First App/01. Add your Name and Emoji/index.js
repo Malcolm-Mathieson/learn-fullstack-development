d9c2c206c47c2f762e7aef9d4befc3df8e144664
Malcolm-Mathieson <malcolm.mathieson@icloud.com>
2026-08-13 23:57:27 +0100

    01. 02. 01.

--- 01. Introduction/02. Your First App/01. Add your Name and Emoji/index.js
+++ 01. Introduction/02. Your First App/01. Add your Name and Emoji/index.js
@@ -79,4 +79,4 @@ const App = (props) => {
 };
 
 // CHALLENGE: add your own name and emoji to the website
-ReactDOM.render(<App name="Someone" emoji="👋"/>, document.getElementById("root"));
+ReactDOM.render(<App name="Malcolm" emoji="🦄"/>, document.getElementById("root"));
